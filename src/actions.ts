6e29875c2708e824ffb7070d335af323fa97acc6
Mayowa Sodipo <sodipomayowa7@gmail.com>
2025-09-12 10:04:55 +0100

    fix:secret

--- src/actions.ts
+++ src/actions.ts
@@ -1,34 +1,33 @@
-'use server'
-import {Stripe} from 'stripe'
+"use server";
+import { Stripe } from "stripe";
 // create stripe checkout session
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!,{
-    apiVersion: '2025-07-30.basil',
-    typescript: true
-})
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY! ?? "", {
+  apiVersion: "2025-07-30.basil",
+  typescript: true,
+});
 export async function createCheckoutSession(
-    email: string,
-    priceId: string,
-    user_id: string,
-    subscription_type: string
-  ) {
-   
-    return JSON.stringify(
-      await stripe.checkout.sessions.create({
-        success_url: `${process.env.NEXT_PUBLIC_URL}/successPage`,
-        cancel_url: `${process.env.NEXT_PUBLIC_URL}/premium`,
-        customer_email: email,
-        line_items: [{ price: priceId, quantity: 1 }],
+  email: string,
+  priceId: string,
+  user_id: string,
+  subscription_type: string
+) {
+  return JSON.stringify(
+    await stripe.checkout.sessions.create({
+      success_url: `${process.env.NEXT_PUBLIC_URL}/successPage`,
+      cancel_url: `${process.env.NEXT_PUBLIC_URL}/premium`,
+      customer_email: email,
+      line_items: [{ price: priceId, quantity: 1 }],
+      metadata: {
+        user_id,
+        subscription_type,
+      },
+      mode: "payment",
+      subscription_data: {
         metadata: {
           user_id,
           subscription_type,
         },
-        mode: 'payment',
-        subscription_data:{
-            metadata:{
-                user_id,
-                subscription_type
-            }
-        }
-      })
-    );
-  }
+      },
+    })
+  );
+}
