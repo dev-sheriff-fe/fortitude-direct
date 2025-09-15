336297146dede201243769999a7e53766f70bf26
dev.sheriff <dev.sheriff.fe@gmail.com>
2025-09-15 15:01:08 +0100

    fix: base url

--- src/utils/fetch-function-auth.ts
+++ src/utils/fetch-function-auth.ts
@@ -4,7 +4,7 @@ import { logout } from './auth-utils';
 const axiosInstanceNoAuth = axios.create({
   baseURL:
     process.env.NEXT_PUBLIC_REACT_APP_API_URL ??
-    'https://www.fortitudedirect.com/api/mmcp/api/v1/',
+    'https://fortitudedirect.com/api/mmcp/api/v1/',
   headers: {
     'x-source-code': 'HELP2PAY',
      'x-client-id': 'TST03054745785188010772',
