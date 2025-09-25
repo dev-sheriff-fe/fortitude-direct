1805d4571087b3f8a7b668469babfbfc8450ff14
sammy-jeff <okoyechibuike048@gmail.com>
2025-09-25 22:30:11 +0100

    fix: fixed send money form

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -2,70 +2,9 @@ import { LoggedInUser } from '@/types/index';
 import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
 
-const initialUser: LoggedInUser = {
-  accountType: null,
-  address: '',
-  appMenuList: null,
-  appSettingsDto: null,
-  appVersion: null,
-  branchID: '',
-  branchName: '',
-  businessName: '',
-  ccy: '',
-  country: '',
-  creditWallet: false,
-  customerTier: null,
-  deviceID: '',
-  email: '',
-  entityCode: '',
-  entityLogo: '',
-  entityName: '',
-  entityStyle: null,
-  entityType: '',
-  expiryDate: null,
-  externalAccount: null,
-  firstname: null,
-  forcePwdChange: '',
-  fullname: '',
-  hardTokenOtpValid: null,
-  language: null,
-  lastLoginDate: '',
-  medium: null,
-  menuInfoList: null,
-  merchantCode: '',
-  merchantGroupCode: '',
-  messageActionDto: null,
-  metaData: null,
-  mobileNo: '',
-  paymentMethodAllowed: null,
-  photoLinks: null,
-  pwd: '',
-  referalCode: '',
-  refreshToken: null,
-  responseCode: '',
-  responseMessage: '',
-  retryNo: 0,
-  softTokenQrCodeLink: null,
-  storeCode: null,
-  supervisor: '',
-  supervisorPassword: null,
-  terminalId: '',
-  ticketID: '',
-  twoFactorLimit: 0,
-  twoFactorType: '',
-  userID: '',
-  userPermissionList: null,
-  userRole: '',
-  userRoles: null,
-  username: '',
-  walletAcc: '',
-  walletBalance: 0,
-  walletType: 0,
-};
-
 // ✅ Define the correct store shape
 interface UserStore {
-  user: LoggedInUser;
+  user: LoggedInUser | null;
   setUser: (user: LoggedInUser) => void;
   clear: () => void;
 }
@@ -74,9 +13,9 @@ interface UserStore {
 const useUser = create<UserStore>()(
   persist(
     (set) => ({
-      user: initialUser,
+      user: null,
       setUser: (user) => set(() => ({ user })),
-      clear: () => set(() => ({ user: initialUser })),
+      clear: () => set(() => ({ user: null })),
     }),
     {
       name: 'store_admin',
